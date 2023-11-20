59642821c7c89c302d105c1517b69ce2a1a85bbe
djordi10 <djordi.setiawan@gmail.com>
2023-11-21 04:18:39 +0700

    update carousel

--- src/components/QuizTopicsScreen/index.tsx
+++ src/components/QuizTopicsScreen/index.tsx
@@ -12,6 +12,8 @@ import {
   PageCenter,
 } from '../../styles/Global'
 import { ScreenTypes } from '../../types'
+import { Carousel } from 'react-responsive-carousel'; // Ensure you have installed this package
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
 
 import Button from '../ui/Button'
 import ModalWrapper from '../ui/ModalWrapper';
@@ -76,6 +78,16 @@ const SelectButtonText = styled.span`
   }
 `;
 
+const CarouselContainer = styled.div`
+  margin-bottom: 20px;
+`;
+
+const CarouselImage = styled.img`
+  width: 100%; /* Adjust width to fill the container */
+  height: 450px; /* Fixed height for all images */
+  object-fit: cover; /* Ensures images cover the area without stretching */
+`;
+
 const QuizTopicsScreen: React.FC = () => {
   const { setCurrentScreen, currentStep } = useQuiz();
   const [showPopup, setShowPopup] = useState(false);
@@ -115,6 +127,22 @@ const QuizTopicsScreen: React.FC = () => {
     <PageCenter>
       <CenterCardContainer>
         <Heading>Penulisan Definisi</Heading>
+        <CarouselContainer>
+          <Carousel autoPlay interval={3000} infiniteLoop showThumbs={false} showStatus={false}>
+            <div>
+              <CarouselImage src="/image/rose.jpg" alt="Mawar" />
+              <p className="legend">Apakah Mawar itu?</p>
+            </div>
+            <div>
+              <CarouselImage src="/image/mahasiswa.jpg" alt="Mahasiswa" />
+              <p className="legend">Apakah Mahasiswa itu?</p>
+            </div>
+            <div>
+              <CarouselImage src="/image/cat.jpg" alt="Kucing" />
+              <p className="legend">Apakah Kucing itu?</p>
+            </div>
+          </Carousel>
+        </CarouselContainer>
         <DetailText>Pilih Step berikut untuk mengerjakan</DetailText>
         <SelectButtonContainer>
           <SelectButton
